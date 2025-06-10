1bab793a84ec7b8f843e80b19328fc7d4959508e
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-06-10 16:10:20 +0100

    added ability to register new user account

--- my_project/recipes/views.py
+++ my_project/recipes/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ProteinRecipes
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -16,7 +19,28 @@ def recipes(request):
 
 
 def login(request):
-    return render(request, 'recipes/login.html')
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        if 'register' in request.POST:
+            form = UserCreationForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get('username')
+                password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=password)
+                login(request, user)
+                return redirect('home')
+        elif 'login' in request.POST:
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'recipes/login.html', {'form': form})
 
 
 def my_account(request):
