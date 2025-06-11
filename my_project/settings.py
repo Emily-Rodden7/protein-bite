00645515c2a1ce4ee30b5ed50e06cb9ea2368500
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-06-11 15:03:14 +0100

    Trying to make images load on the recipe page.

--- my_project/settings.py
+++ my_project/settings.py
@@ -122,3 +122,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# added for upload of images
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'recipe_images')
