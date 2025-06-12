cd5a4acab1126fdedda2b9a164529bb2c19ba4f1
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-06-12 15:21:59 +0100

    final edits and touch ups to the log in page.

--- proteinbite/views.py
+++ proteinbite/views.py
@@ -64,7 +64,6 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            messages.success(request, f"Welcome back, {username}!")
             return redirect('account')
         else:
             messages.error(request, "Invalid username or password.")
