51f8dc73dbe918fda2f2b850262f1abc8bbb0a0d
Emily-Rodden7 <dictbyzn@students.codeinstitute.net>
2025-06-12 10:08:57 +0100

    Readded superuser / admin login.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\emily.rodden\OneDrive - WDT, inc SfH, SFJA, SFJ and P1st\Documents\protein-bite\my_project\venv"
+set "VIRTUAL_ENV=C:\Users\emily.rodden\OneDrive - WDT, inc SfH, SFJA, SFJ and P1st\Documents\protein-bite\venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
