c2286b6b4ff7753411f072afe2fba5fd9f9cbe2a
Emily Rodden <emilyrodden.cg@gmail.com>
2025-10-20 17:00:16 +0100

    created .env and added to gitignore.

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jy!+-ijk1^wh4#&v^i#^mlxzmwph86ry$iavz3+jy*cxob3gpm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
